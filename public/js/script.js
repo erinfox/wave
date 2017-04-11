a9232f6d498e6bc6eb0bcca3ef669fe3620ecabe
erin fox <erinkayfox@gmail.com>
2017-04-11 15:59:56 -0400

    everything but the last api

--- public/js/script.js
+++ public/js/script.js
@@ -16,15 +16,16 @@ var getBreakInfo = function(break_location){
       url: "http://api.wunderground.com/api/5318c4345dfafdf3/tide/q/CA/"+ breakName +".json",
       method: "GET",
       success: function(obj){
-        // console.log("success obj", obj)
+        console.log("success obj", obj)
     let time_date = obj.tide.tideSummary[0].date.pretty
     let high_tide_height = obj.tide.tideSummary[0].data.height
     // let high_tide_type = obj.tide.tideSummary[0].data.type
     let low_tide_height = obj.tide.tideSummary[5].data.height
     // let low_tide_type = obj.tide.tideSummary[5].data.type
 
-      breakLocation.html("Date & Time: " + time_date
-        + " High Tide: " + high_tide_height
+
+      breakLocation.html("Date & Time: " + time_date +"<br>"
+        + " High Tide: " + high_tide_height +"<br>"
         + " Low Tide: " + low_tide_height)
 
         // console.log(obj.tide.tideSummary[0].date.pretty);
@@ -39,25 +40,6 @@ var getBreakInfo = function(break_location){
 }; // getBreakInfo
 getBreakInfo();
 
-
-
-
-// let forecast = function(forecast_image){
-//   var breakName = $("#favBreak").attr('breakname');
-// console.log(breakName)
-// $.ajax({
-//   url: "http://api.wunderground.com/api/5318c4345dfafdf3/conditions/q/CA/" +breakName+ ".json",
-//   method: "GET",
-//   success: function(dataInfo){
-//     console.log("success dataInfo", dataInfo)
-//     let conditions = dataInfo.current_observation[0].estimated[1].forecast_url
-
-//     forecastLocation.html(conditions)
-
-//   }//success
-// });//ajax
-// };//forecast
-// forecast();
 
 
 }); //ends doc.ready
